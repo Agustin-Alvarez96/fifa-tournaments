8736b8ba8f5ec92943c2b764081e9c7ad8e7668a
AgusAlvarez <agustinmalvarezok@outlook.com>
2025-07-31 00:12:15 -0300

    feature: first commit - spring structure

---
+++ src/main/java/com/my_proyects/fifatournaments/FifaTournamentsApplication.java
@@ -0,0 +1,13 @@
+package com.my_proyects.fifatournaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaTournamentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaTournamentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my_proyects/fifatournaments/FifaTournamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_proyects.fifatournaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaTournamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
